a254e2c6266397485986fb97f4beba7eda4a86ac
Khaled Mahmoud <khaledoud909@gmail.com>
2026-02-09 20:43:38 +0300

    remove unused imports

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import { useDocumentHead } from '../hooks/useDocumentHead';
